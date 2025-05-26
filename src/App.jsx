b24dca5e3b5c6f5afce36b81b99a80d444cc56bf
Usman Ali <127815926+u5man-ali@users.noreply.github.com>
2025-05-26 13:05:22 +0500

    changed location of logo.svg

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="w-full justify-center flex flex-wrap gap-4 min-h-screen bg-white-200 text-black-800 p-6 max-w-6xl mx-auto">
       <div className="w-full h-min flex items-center gap-4 bg-aqua-50 justify-center p-6 rounded-curve-xxl">
-      <img src="src/DezineCrafts.svg" alt="Dezine Crafts Logo" className="w-12 h-12"></img>
+      <img src="./DezineCrafts.svg" alt="Dezine Crafts Logo" className="w-12 h-12"></img>
       <h1 className="text-disp-sm font-semibold">DC Ultimate UI Kit - Live Preview</h1></div>
       <section className="w-full mb-12">
         <h2 className="text-xl font-semibold mb-4">Button with both icons</h2>
