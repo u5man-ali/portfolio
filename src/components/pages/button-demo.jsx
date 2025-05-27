296b4f72c784c1363cd3d866272d8a931e5e0541
Usman Ali <127815926+u5man-ali@users.noreply.github.com>
2025-05-27 22:55:59 +0500

    updated button design

--- src/components/pages/button-demo.jsx
+++ src/components/pages/button-demo.jsx
@@ -6,6 +6,35 @@ import IconRight from '../../icons/Arrow-Enter-Left-Outline.svg?react'
 export default function buttonDemo() {
   return (
     <div className="flex justify-center items-center gap-4 mb-4 flex-wrap">
+        <div className="w-full flex justify-center items-center gap-4 mb-4 flex-wrap">
+        <h3 className="w-full h-min text-text-lg font-semibold">Neutral Button</h3>
+        <Button
+        size="sm"
+        variant="neutral"
+        leftIcon={IconLeft}
+        rightIcon={IconRight}
+        >
+        Neutral Small
+        </Button>
+        <Button
+        size="md"
+        variant="neutral"
+        showLeftIcon={false}
+        showRightIcon={false}
+        leftIcon={IconLeft}
+        rightIcon={IconRight}
+        >
+        Neutral No Icons
+        </Button>
+        <Button
+        size="lg"
+        variant="neutral"
+        leftIcon={IconLeft}
+        rightIcon={IconRight}
+        >
+        Neutral Large
+        </Button>
+        </div>
       <h3 className="w-full h-min text-text-lg font-semibold">Primary Button</h3>
         <Button
         size="sm"
@@ -32,5 +61,6 @@ export default function buttonDemo() {
         Large Button
         </Button>
     </div>
+    
   );
 }
