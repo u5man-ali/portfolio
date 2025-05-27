8d33fc9dcca90a3a51800db8ce0969cf02240dd0
Usman Ali <127815926+u5man-ali@users.noreply.github.com>
2025-05-27 23:02:44 +0500

    button style update

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -10,7 +10,7 @@ const sizes = {
 const variants = {
   primary: "bg-aqua-600 text-white-50 hover:bg-aqua-500 focus:ring-aqua-300",
   secondary: "bg-gray-300 text-black-800 hover:bg-gray-200 focus:ring-gray-300",
-  neutral: "border align-middle select-none text-center duration-300 ease-in focus:shadow-none focus:ring-gray-500 text-sm bg-black-100 hover:bg-gray-800 relative bg-gradient-to-b from-black-100 to-black-300 border-black-300 text-white-50 hover:bg-gradient-to-b hover:from-gray-800 hover:to-gray-950 hover:border-gray-950 after:absolute after:inset-0 after:rounded-[inherit] after:shadow-[inset_0_1px_0px_rgba(255,255,255,0.25),inset_0_-2px_0px_rgba(0,0,0,0.25)]",
+  neutral: "border align-middle select-none text-center duration-300 ease-in focus:shadow-none focus:ring-gray-500 text-sm bg-black-100 hover:bg-gray-800 relative bg-gradient-to-b from-black-100 to-black-300 border-black-300 text-white-50 hover:bg-gradient-to-b hover:from-gray-800 hover:to-gray-950 hover:border-gray-950 after:absolute after:inset-0 after:rounded-[inherit] after:shadow-[inset_1px_1px_3px_rgba(255,255,255,0.25),inset_0_-2px_0px_rgba(0,0,0,0.25)]",
   success: "bg-green-600 text-white-50 hover:bg-green-500 focus:ring-green-300",
   error: "bg-red-600 text-white-50 hover:bg-red-500 focus:ring-red-300",
   warning: "bg-yellow-600 text-white-50 hover:bg-yellow-500 focus:ring-yellow-300",
