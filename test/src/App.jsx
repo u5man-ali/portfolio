96d9029713c8afe84630632ae7c23f98c8ae9d63
Usman Ali <127815926+u5man-ali@users.noreply.github.com>
2025-05-31 16:28:40 +0500

    removed buttons and added urdu text

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -13,53 +13,7 @@ function App() {
         <h2 className="text-2xl font-semibold mb-4">Button with both icons</h2>
         <div className="flex gap-4 flex-wrap">
           {/*Show both icons */}
-          <Button
-            size="md"
-            variant="primary"
-            leftIcon={IconLeft}
-            rightIcon={IconRight}
-          >
-          With Icons
-          </Button></div>
-          <h2 className="text-2xl font-semibold mb-4">Button with only right side icon</h2>
-          <div>
-          {/*Show right icon only */}
-          <Button
-            size="md"
-            variant="secondary"
-            showLeftIcon={false}
-            leftIcon={IconLeft}
-            showRightIcon={true}
-            rightIcon={IconRight}            
-          >
-          Right Only
-          </Button></div>
-          <h2 className="text-2xl font-semibold mb-4">Button with only left side icon</h2>
-          <div>
-          {/*Show left icon only */}
-          <Button
-            size="md"
-            variant="success"
-            showLeftIcon={true}
-            leftIcon={IconLeft}
-            showRightIcon={false}
-            rightIcon={IconRight}
-          >
-          Left Only
-          </Button></div>
-          <h2 className="text-2xl font-semibold mb-4">Button without any icons</h2>
-          <div>
-          {/* Hide both icons */}
-          <Button
-            size="md"
-            variant="neutral"
-            showLeftIcon={false}
-            leftIcon={IconLeft}
-            showRightIcon={false}
-            rightIcon={IconRight}            
-          >
-          No Icons
-          </Button>
+          <p className="rtl:text-right rtl:flex-row-reverse text-lg font-urdu font-bold mb-6">شبان المسلمین ایک تحریک کا نام ہے جو نوعِ انسانی کے سامنے اسلام کے عادلانہ انقلابی تصور کو پیش کرنا چاہتی ہے اور اس انقلابی تصور کی بنیاد پر ایسا معاشرہ تشکیل دینے کا پروگرام بھی اپنے پاس رکھتی ہے جس میں آج کے دور کے تمام جدید مسائل کے قرآن و سنت کی روشنی میں حل پیش کیے گئے ہوں</p>
         </div>
       </section>
       {/* Place your components here */}
